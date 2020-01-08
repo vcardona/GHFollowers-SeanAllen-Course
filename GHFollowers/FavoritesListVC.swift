8917d1b027869da8fc5f6364e615737986c06795
Victor Cardona <victorhugocardonagomez@gmail.com>
2020-01-08 14:06:46 -0500

    Initial Commit

--- GHFollowers/FavoritesListVC.swift
+++ GHFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
 //  Created by Victor Hugo on 8/01/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+
     }
-
-
+    
 }
-
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Victor Hugo on 8/01/20.
+//  Copyright © 2020 Vintage Robot. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+}
